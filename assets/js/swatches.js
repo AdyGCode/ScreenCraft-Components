3358ce41ddd0dd7e7d511a83f300366978b26904
AdyGCode <Adrian.Gould@nmtafe.wa.edu.au>
2023-11-19 11:45:23 +0800

    feat(colours): Add detail to explanation.
    - Move explanation into separate section.
    - Prepare section for colour counts
    - Add code to JS to add colour counts

--- assets/js/swatches.js
+++ assets/js/swatches.js
@@ -1,31 +1,37 @@
-function updateNumbersOfColours(base = 22, count = 0) {
+const baseColourCount = 22
+
+function updateNumbersOfColours(base = baseColourCount, count = 0) {
     const colourCount = document.getElementById('additionalColours')
     const baseCount = document.getElementById('baseColours')
-    colourCount.innerText = `{count-base} Custom colours.`
-    baseCount.innerText = `{base} Base colours.`
+
+    colourCount.innerText = `${count - baseColourCount} Custom colours.`
+    baseCount.innerText = `${baseColourCount} Base colours.`
 }
 
-function generateSwatches(colours) {
+function generateSwatches(baseCount = baseColourCount, colours) {
     const swatchesContainer = document.getElementById('swatches')
-
     swatchesContainer.innerHTML = ''
+
+    let swatchCount = 0
 
     if (typeof colours !== "array") {
         for (const colourName in colours) {
             const colourShades = colours[colourName]
+            swatchCount++
+
             let swatchSection = document.createElement('section')
             swatchSection.classList.add("flex")
             swatchSection.classList.add("flex-col")
             swatchSection.classList.add("w-full")
-            swatchSection.classList.add("m-4")
             swatchSection.classList.add("p-0")
             swatchSection.classList.add("border")
-            swatchSection.classList.add("border-gray-500")
+            // swatchSection.classList.add(swatchCount > baseColourCount ? 'border-gray-700' : 'border-black')
             swatchSection.classList.add("shadow")
+
             let heading = document.createElement('h3')
             heading.textContent = colourName;
             heading.classList.add('w-full')
-            heading.classList.add('bg-black')
+            heading.classList.add(swatchCount > baseColourCount ? 'bg-gray-600' : 'bg-black')
             heading.classList.add('text-white')
             heading.classList.add('text-center')
             heading.classList.add('font-bold')
@@ -45,10 +51,10 @@ function generateSwatches(colours) {
                                    <p class="col-span-2 px-2" style="background:${colourShades[shade]}">${colourShades[shade]}</p>`
 
                 swatchSection.appendChild(paragraph)
-        }
+            }
 
             swatchesContainer.appendChild(swatchSection)
-    }
+        }
     }
 }
 
@@ -56,21 +62,28 @@ function readAndUpdateColours() {
     fetch('../colours/colours.json')
         .then(response => response.json())
         .then(data => {
+            if (localStorage.getItem("colours") == null) {
+                localStorage.setItem('colours', JSON.stringify([]))
+            }
+            if (localStorage.getItem("colourCount") == null) {
+                localStorage.setItem('colourCount', JSON.stringify(0))
+            }
+
             const storedColours = JSON.parse(localStorage.getItem('colours'))
             const storedColourCount = JSON.parse(localStorage.getItem('colourCount'))
+
+            const colours = data.colors;  // arg American spelling in TailwindCSS
+            const colourCount = Object.keys(data.colors).length;  // arg American spelling in TailwindCSS
 
             if (storedColours !== colours) {
                 localStorage.setItem('colours', JSON.stringify(colours))
             }
-            generateSwatches(colours);
-
-            const colours = data.colours;
-            const colourCount = data.colours.length;
+            generateSwatches(baseColourCount, colours);
 
             if (storedColourCount !== colourCount) {
                 localStorage.setItem('colourCount', JSON.stringify(colourCount))
             }
-            updateNumbersOfColours(colourCount);
+            updateNumbersOfColours(baseColourCount, colourCount);
 
         })
         .catch(error => {
